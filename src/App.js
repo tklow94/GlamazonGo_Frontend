bb2b9dd12b3ce5843eeca59cd444244d5be776ec
Trevor <tlow94@uw.edu>
2020-11-16 16:21:01 -0800

    user state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import {Provider} from 'react-redux'
+import store from './redux/store'
+import UserContainer from './components/UserContainer'
 import './App.css';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <UserContainer/>
     </div>
+    </Provider>
   );
 }
 
