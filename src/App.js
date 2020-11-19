b912a16428d3717cfebb86d60fa2d56c56de4c99
Trevor <tlow94@uw.edu>
2020-11-18 17:14:56 -0800

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Router, Switch, Route, Link } from "react-router-dom";
 import {Provider} from 'react-redux'
+import { history } from "./redux/helpers/history";
+
 import store from './redux/store'
 import './App.css';
-import Login from './components/Login'
+
+
+import Login from "./components/Login";
+// import Register from "./components/Register";
+import Profile from "./components/Profile";
+
+
+
 
 function App() {
   return (
     <Provider store={store}>
+      <Router history={history}>
     <div className="App">
-   <Login/>
+    <Switch>
+   <Route exact path="/login" component={Login}/>
+   <Route exact path="/profile" component={Profile}/>
+   </Switch>
     </div>
+    </Router>
     </Provider>
   );
 }
