7cc245b7b4934bff44f180ba968f7f689f5687d2
Trevor <tlow94@uw.edu>
2020-11-23 19:33:43 -0800

    home page code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import UserService from "../services/user.service";
 
-export default Home
+const Home = () => {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    UserService.getPublicContent().then(
+      (response) => {
+        setContent(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+
+        setContent(_content);
+      }
+    );
+  }, []);
+
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>{content}</h3>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
